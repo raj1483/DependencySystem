6983ee2c2f792960b4ffe8eef15566fd6bc05c8f
raj1483 <117981560+raj1483@users.noreply.github.com>
2026-02-05 02:37:25 +0530

    controller

---
+++ Controllers/Auth/AuthController.cs
@@ -0,0 +1,79 @@
+﻿using DependencySystem.DTOs.Auth;
+using DependencySystem.Services.Auth;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DependencySystem.Controllers.Auth
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterRequestDto dto)
+        {
+            var result = await _authService.RegisterAsync(dto);
+            if (!result.Success) return BadRequest(result);
+            return Ok(result);
+        }
+
+        [HttpPost("send-otp")]
+        public async Task<IActionResult> SendOtp(SendOtpRequestDto dto)
+        {
+            var result = await _authService.SendOtpAsync(dto);
+            if (!result.Success) return BadRequest(result);
+            return Ok(result);
+        }
+
+        [HttpPost("verify-otp")]
+        public async Task<IActionResult> VerifyOtp(VerifyOtpRequestDto dto)
+        {
+            var result = await _authService.VerifyOtpAsync(dto);
+            if (!result.Success) return BadRequest(result);
+            return Ok(result);
+        }
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(LoginRequestDto dto)
+        {
+            var result = await _authService.LoginAsync(dto);
+
+            if (!result.Success)
+                return BadRequest(result);
+
+            return Ok(result);
+        }
+        [HttpPost("refresh-token")]
+        public async Task<IActionResult> RefreshToken(RefreshTokenRequestDto dto)
+        {
+            var result = await _authService.RefreshTokenAsync(dto);
+
+            if (!result.Success)
+                return BadRequest(result);
+
+            return Ok(result);
+        }
+        [HttpPost("forgot-password")]
+        public async Task<IActionResult> ForgotPassword(ForgotPasswordRequestDto dto)
+        {
+            var result = await _authService.ForgotPasswordAsync(dto);
+            if (!result.Success) return BadRequest(result);
+            return Ok(result);
+        }
+
+        [HttpPost("reset-password")]
+        public async Task<IActionResult> ResetPassword(ResetPasswordRequestDto dto)
+        {
+            var result = await _authService.ResetPasswordAsync(dto);
+            if (!result.Success) return BadRequest(result);
+            return Ok(result);
+        }
+
+
+    }
+}
---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,32 @@
+﻿using DependencySystem.Helper;
+//using DependencySystem.Helper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DependencySystem.Controllers
+{
+    [Route("api/dashboard")]
+    [ApiController]
+    public class DashboardController : ControllerBase
+    {
+        [HttpGet("admin")]
+        //[Authorize(Roles = AppRoles.Admin)]
+        public IActionResult AdminDashboard()
+            => Ok("✅ Welcome Admin Dashboard");
+
+        [HttpGet("manager")]
+        //[Authorize(Roles = AppRoles.Manager)]
+        public IActionResult ManagerDashboard()
+            => Ok("✅ Welcome Manager Dashboard");
+
+        [HttpGet("developer")]
+        //[Authorize(Roles = AppRoles.Developer)]
+        public IActionResult DeveloperDashboard()
+            => Ok("✅ Welcome Developer Dashboard");
+
+        [HttpGet("maintainer")]
+        //[Authorize(Roles = AppRoles.Maintainer)]
+        public IActionResult MaintainerDashboard()
+            => Ok("✅ Welcome Maintainer Dashboard");
+    }
+}
